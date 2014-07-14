fe5a6dac3fdf47658504f93d094ad843fbfd175c
Wing Hym Liu <winghymliu@gmail.com>
2014-07-14 22:51:38 +0100

    Update Test Cases
    Made custom exceptions
    Fixed test cases

--- src/Calculator.scala
+++ src/Calculator.scala
@@ -8,11 +8,22 @@ case object Plus extends Input {
   override def toString = "+"
 }
 
+class UnknownTokenException(message: String = null) extends RuntimeException(message)
+class StackException(message: String = null) extends RuntimeException(message)
+
 class Calculator {
 	
 	def calculate(inputs : List[Input]) = {
-		val stack = new Stack[Double]();
+	  	 
+	  if (inputs.length == 1){
+	    inputs.head match {
+	    	case Num(x) => x	    	  
+	    	case _ => throw new Exception()	    	  
+	    } 	        	      
+	  } else {
+	    val stack = new Stack[Double]();
 		eval(stack, inputs)
+	  }	     		
 	}
 	
 	def eval(stack: Stack[Double], inputs: List[Input]) : Double ={
@@ -24,11 +35,14 @@ class Calculator {
 	  	  eval(stack.push(x), xs)
 	  	case Plus :: xs =>
 	  	  eval(addition(stack), xs)
-	  	case _ => throw new Exception() 
+	  	case _ => throw new UnknownTokenException("Invalid RPN inputs") 
 	  }
 	}
 	
 	def addition(stack:Stack[Double]) : Stack[Double] ={
+		if(stack.length < 2)
+		  throw new StackException("Insufficient arguments in the stack to do addition")
+	  
 		val (left, newStack1) = stack.pop2
 	    val (right, newStack2) = newStack1.pop2
 	    val res = left + right
