74b81bf10dda423b59cb8151cc3d1b585d064921
Wing Hym Liu <Presenter@fionas-mbp.home>
2014-07-15 21:49:46 +0100

    Format and more addition tests

--- src/Calculator.scala
+++ src/Calculator.scala
@@ -12,40 +12,39 @@ class UnknownTokenException(message: String = null) extends RuntimeException(mes
 class StackException(message: String = null) extends RuntimeException(message)
 
 class Calculator {
-	
-	def calculate(inputs : List[Input]) = {
-	  	 
-	  if (inputs.length == 1){
-	    inputs.head match {
-	    	case Num(x) => x	    	  
-	    	case _ => throw new Exception()	    	  
-	    } 	        	      
-	  } else {
-	    val stack = new Stack[Double]();
-		eval(stack, inputs)
-	  }	     		
-	}
-	
-	def eval(stack: Stack[Double], inputs: List[Input]) : Double ={
-	  inputs match
-	  {
-	    case Nil => 
-	      stack.head
-	  	case Num(x) :: xs => 
-	  	  eval(stack.push(x), xs)
-	  	case Plus :: xs =>
-	  	  eval(addition(stack), xs)
-	  	case _ => throw new UnknownTokenException("Invalid RPN inputs") 
-	  }
-	}
-	
-	def addition(stack:Stack[Double]) : Stack[Double] ={
-		if(stack.length < 2)
-		  throw new StackException("Insufficient arguments in the stack to do addition")
-	  
-		val (left, newStack1) = stack.pop2
-	    val (right, newStack2) = newStack1.pop2
-	    val res = left + right
-	    newStack2.push(res)
-	} 
+
+  def calculate(inputs: List[Input]) = {
+
+    if (inputs.length == 1) {
+      inputs.head match {
+        case Num(x) => x
+        case _ => throw new Exception()
+      }
+    } else {
+      val stack = new Stack[Double]();
+      eval(stack, inputs)
+    }
+  }
+
+  def eval(stack: Stack[Double], inputs: List[Input]): Double = {
+    inputs match {
+      case Nil =>
+        stack.head
+      case Num(x) :: xs =>
+        eval(stack.push(x), xs)
+      case Plus :: xs =>
+        eval(addition(stack), xs)
+      case _ => throw new UnknownTokenException("Invalid RPN inputs")
+    }
+  }
+
+  def addition(stack: Stack[Double]): Stack[Double] = {
+    if (stack.length < 2)
+      throw new StackException("Insufficient arguments in the stack to do addition")
+
+    val (left, newStack1) = stack.pop2
+    val (right, newStack2) = newStack1.pop2
+    val res = left + right
+    newStack2.push(res)
+  }
 }
--- src/tests/CalculatorTests.scala
+++ src/tests/CalculatorTests.scala
@@ -2,30 +2,43 @@ import org.scalatest._
 
 class CalculatorTests extends FlatSpec with Matchers {
   val calculator = new Calculator
-  
+
   "A calculator" should "just return a single number from a single number input" in {
-      val input = List(Num(1.0)) 
-	  val result = calculator.calculate(input)
-	  result should be (1.0)
+    val input = List(Num(1.0))
+    val result = calculator.calculate(input)
+    result should be(1.0)
   }
-  
+
   it should "be able to add two postive numbers" in {
-	  val input = List(Num(1.0), Num(2.0), Plus) 
-	  val result = calculator.calculate(input)
+    val input = List(Num(1.0), Num(2.0), Plus)
+    val result = calculator.calculate(input)
+    result should be (3.0)
   }
   
+  it should "be able to add three postive numbers" in {
+    val input = List(Num(1.0), Num(2.0), Num(3.0), Plus, Plus)
+    val result = calculator.calculate(input)
+    result should be (6.0)
+  }
+  
+  it should "be able to add four postive numbers" in {
+    val input = List(Num(1.0), Num(2.0), Num(3.0), Num(4.0), Plus, Plus, Plus)
+    val result = calculator.calculate(input)
+    result should be (10.0)
+  } 
+
   it should "throw an exception from an invalid PN to add two numbers" in {
-	  val input = List(Num(1.0), Plus, Num(2.0)) 
-	  intercept[StackException]{
-	    calculator.calculate(input)	    
-	  }
+    val input = List(Num(1.0), Plus, Num(2.0))
+    intercept[StackException] {
+      calculator.calculate(input)
+    }
   }
-  
+
   it should "throw an exception for any input with the incorrect number of operands and numbers" in {
-	  val input = List(Num(1.0), Num(2.0), Plus, Plus) 
-	  intercept[StackException]{
-		  calculator.calculate(input)
-	  }
+    val input = List(Num(1.0), Num(2.0), Plus, Plus)
+    intercept[StackException] {
+      calculator.calculate(input)
+    }
   }
- 
+
 }
